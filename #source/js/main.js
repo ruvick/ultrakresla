8ddd152ae91aa3a72a6b15bf992da5fb0ca3db17
ruvick <rudikov.web@ya.ru>
2021-03-22 15:04:06 +0300

    Стартовые файлы

---
+++ #source/js/main.js
@@ -0,0 +1,8 @@
+//@prepros-append jq-start.js
+//@prepros-append forms.js
+//@prepros-append jquery.inputmask.bundle.min.js
+//@prepros-append jquery.fancybox.min.js
+//@prepros-append slick.min.js
+//@prepros-append script.js
+//@prepros-append jq-end.js    
+ 
